ed4abe584bd3f90af81febfcd5757ce7f86befad
Jinsong <njs145@naver.com>
2024-11-20 23:56:58 +0900

    [WIP] server client 방식으로 구성.
    - server, client 간의 초기화 코드 구성 완료.
    - recv 스레드 구현 진행 중.

--- main.c
+++ main.c
@@ -10,11 +10,15 @@
 #define SHM_NAME "/my_shared_memory"
 #define SEM_NAME "/my_semaphore"
 
+#define MAX_USER    12
+
 typedef struct 
 {
     __uint32_t pid;
     __uint32_t recv_flag;
     __uint32_t recv_player;
+    sem_t *user_sem[MAX_USER];
+    char sem_name[MAX_USER][255];
     char data[1024];
 }shared_data;
 
@@ -92,7 +96,8 @@ void writer_process()
     sem_close(sem);
 }
 
-void reader_process() {
+void recv_thread(sem_t *user_sem) 
+{
     // 공유 메모리 및 세마포어 열기
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
     shared_data *g_shared_mem = mmap(0, sizeof(shared_data), O_RDWR, MAP_SHARED, shm_fd, 0);
@@ -106,7 +111,7 @@ void reader_process() {
     do
     {
         /* 세마포어 알림 기다리기. */
-        sem_wait(g_sem);
+        sem_wait(user_sem);
 
         /* 알림 받은 후 읽기. */
         printf("%d: %s\n", g_shared_mem->pid, g_shared_mem->data);
@@ -120,19 +125,79 @@ void reader_process() {
 
 int main(int argc, char* argv[]) 
 {
-    pthread_t thread;
+    int shm_fd;
+    if(argc > 1)
+    {
+        if(strcmp(argv[1], "server") == 0)
+        {
+            sem_t *server_sem;
+            shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+            if(shm_fd == -1)
+            {
+                printf("create shared mem!\n");
+                shm_fd = shm_open(SHM_NAME, (O_CREAT | O_RDWR), 0666);
+                ftruncate(shm_fd, sizeof(g_shared_mem));  // 공유 메모리의 사이즈를 설정한다.
+                g_shared_mem =mmap(0, sizeof(shared_data), O_RDWR, MAP_SHARED, shm_fd, 0);
 
-    if (argc < 2) {
-        fprintf(stderr, "사용법: %s [writer|reader]\n", argv[0]);
-        exit(1);
-    }
+            }
+            else
+            {
+                printf("Close previous shared mem, semaphore!\n");
+                // shm_unlink(SHM_NAME);
+                // if(sem_close(server_sem) == -1)
+                // {
+                    // perror("sem_close failed!\n");
+                // }
+                // if(sem_unlink(SEM_NAME) == -1)
+                // {
+                    // perror("sem_unlink failed!\n");
+                // }
+                close(shm_fd);
 
-    if (strcmp(argv[1], "writer") == 0) {
-        writer_process();
-    } else if (strcmp(argv[1], "reader") == 0) {
-        pthread_create(&thread, NULL, input_thread, NULL);
-        reader_process();
+                printf("Open new shared mem, semaphore!\n");
+
+                shm_fd = shm_open(SHM_NAME, (O_CREAT | O_RDWR), 0666);
+                ftruncate(shm_fd, sizeof(g_shared_mem));  // 공유 메모리의 사이즈를 설정한다.
+                g_shared_mem =mmap(0, sizeof(shared_data), O_RDWR, MAP_SHARED, shm_fd, 0);
+            }
+        }
+        else if(strcmp(argv[1], "user") == 0)
+        {
+            char sem_name[255];
+
+            shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+            if(shm_fd == -1)
+            {
+                perror("server is close! please create server\n");
+                exit(1);
+            }
+
+            sem_t *user_sem;
+            
+            g_shared_mem =mmap(0, sizeof(shared_data), O_RDWR, MAP_SHARED, shm_fd, 0);
+            sprintf(sem_name, "user sem %d",g_shared_mem->recv_player);
+            
+            user_sem = sem_open(sem_name, 0);
+
+            g_shared_mem->user_sem[g_shared_mem->recv_player] = user_sem;
+            strcpy(g_shared_mem->sem_name[g_shared_mem->recv_player], sem_name);
+
+        }
+        else
+        {
+            printf("please select server or user!\n");
+            printf("Usage ./main server or user\n");
+        }
     }
+    else
+    {
+        printf("Usage ./main server or user\n");
+    }
+        
+
+    munmap(g_shared_mem, sizeof(shared_data));
+    shm_unlink(SHM_NAME);
+    close(shm_fd);
 
     return 0;
 }
